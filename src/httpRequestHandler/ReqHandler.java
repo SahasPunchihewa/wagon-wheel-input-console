7291a3f447c9f78810eb3c323b1ca36bf38e82f7
sashin2000 <sashinsirichandra@gmail.com>
2020-03-11 23:00:22 +0530

    created methods for file handling part

--- src/httpRequestHandler/ReqHandler.java
+++ src/httpRequestHandler/ReqHandler.java
@@ -1,17 +1,27 @@
 package httpRequestHandler;
 
-public class ReqHandler extends MCG.WWheal{
+public class ReqHandler{
 
-	static int x;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(xScore);
-		nama();
-
+	public static void addShot(int playerID, String playerTeam,int playerSession,int scoreShot, float scoreX, float scoreY){
+		OkHttpClient client = new OkHttpClient().newBuilder()
+			.build();
+		MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+		RequestBody body = RequestBody.create(mediaType, "playerID="+playerID+"&playerTeam="+playerTeam+"&playerSession="+playerSession+"&scoreShot="+scoreShot+"&scoreX="+scoreX+"&scoreY="+scoreY);
+		Request request = new Request.Builder()
+			.url("ec2-18-217-163-34.us-east-2.compute.amazonaws.com:8080/api/score")
+			.method("POST", body)
+			.addHeader("Content-Type", "application/x-www-form-urlencoded")
+			.build();
 	}
-	public static void nama()
-	{
-		
+	
+	public static Response playerList(String playerTeam){
+		OkHttpClient client = new OkHttpClient().newBuilder()
+			.build();
+		Request request = new Request.Builder()
+			.url("ec2-18-217-163-34.us-east-2.compute.amazonaws.com:8080/api/player/"+playerTeam)
+			.method("GET", null)
+			.build();
+		Response response = client.newCall(request).execute();
+		return response;
 	}
-
 }
