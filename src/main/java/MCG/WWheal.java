07497bf3409b969ec95ab3dc71e7c6f0a47b5aec
SahasPunchihewa <sahasmcg2995@gmail.com>
2020-03-14 06:28:51 +0530

    Aded Radio Buttons

--- src/main/java/MCG/WWheal.java
+++ src/main/java/MCG/WWheal.java
@@ -1,21 +1,29 @@
-package MCG;
+package main.java.MCG;
 import javax.swing.*;
-
-
+import java.awt.*;
 import java.awt.event.*;
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Font;
-public class WWheal extends JFrame implements MouseMotionListener,ActionListener
+import java.io.*;
+public class WWheal extends JFrame implements MouseMotionListener,ActionListener,ItemListener
 {
 	static JFrame f1,f2;
 	static JLabel l1,l2;
-	static JButton b1,b2,b3,b4,b5;
-	protected static int pid,psession,sshot,xscore,yscore;
-	protected static String pTeam=" ";
+	static JButton b1,b2,b3,b4,b5,b6,b7;
+	static JRadioButton r1,r2,r3,r4;
+	static ButtonGroup rbg;
+	static GraphicsEnvironment ge;
+	static GraphicsDevice screen;
+	static Rectangle rect;
+	static int pid,psession,sshot,xscore,yscore,sx,sy;
+	static String pTeam=" ";
 	
 	public WWheal()
 	{
+		System.out.println("Started");
+		ge=GraphicsEnvironment.getLocalGraphicsEnvironment();
+		screen=ge.getDefaultScreenDevice();
+		rect=screen.getDefaultConfiguration().getBounds();
+		sx=(int)(rect.getMaxX());
+		sy=(int)(rect.getMaxY());
 		f1=new JFrame();
 		f2=new JFrame();
 		b1=new JButton("1");
@@ -23,8 +31,20 @@ public class WWheal extends JFrame implements MouseMotionListener,ActionListener
 		b3=new JButton("3");
 		b4=new JButton("4");
 		b5=new JButton("6");
+		b6=new JButton("X");
+		b7=new JButton("_");
+		r1=new JRadioButton("MCG 1st");
+		r2=new JRadioButton("RCG 1st");
+		r3=new JRadioButton("MCG 2nd");
+		r4=new JRadioButton("RCG 2nd");
+		rbg=new ButtonGroup();
+		rbg.add(r1);
+		rbg.add(r2);
+		rbg.add(r3);
+		rbg.add(r4);
 		l1=new JLabel();
 		l2=new JLabel("Plaese Select a Type");
+		l1.setIcon(new ImageIcon(WWheal.class.getResource("/main/java/IMG/Ground.png")));
 		l2.setHorizontalAlignment(SwingConstants.CENTER);
 		l2.setFont(new Font("Century Gothic", Font.BOLD, 20));
 		l1.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -35,12 +55,23 @@ public class WWheal extends JFrame implements MouseMotionListener,ActionListener
 		b3.setBounds(167,85,50,50);
 		b4.setBounds(239,85,50,50);
 		b5.setBounds(311,85,50,50);
+		b6.setBounds(1870,0,50,30);
+		b7.setBounds(1821,0,50,30);
+		r1.setBounds(19,205,72,50);
+		r2.setBounds(110,205,72,50);
+		r3.setBounds(201,205,72,50);
+		r4.setBounds(292,205,72,50);
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		b3.addActionListener(this);
 		b4.addActionListener(this);
 		b5.addActionListener(this);
-		l1.setIcon(new ImageIcon(WWheal.class.getResource("/IMG/Ground.png")));
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		r1.addItemListener(this);
+		r2.addItemListener(this);
+		r3.addItemListener(this);
+		r4.addItemListener(this);
 		l1.addMouseListener(new MouseAdapter()
 		{
 			public void mousePressed(MouseEvent e)
@@ -55,8 +86,19 @@ public class WWheal extends JFrame implements MouseMotionListener,ActionListener
 			}
 		}
 		);
-		//selectui(f2);
-		mainui(f1);
+		selectui(f2);
+		//mainui(f1);
+	}
+	public void itemStateChanged(ItemEvent f) 
+	{
+		if(f.getItemSelectable()==rbg)
+		{
+			System.out.println("MCG 1");
+		}
+		if(f.getItemSelectable()==rbg)
+		{
+			System.out.println("RCG 1");
+		}
 	}
 	public void actionPerformed(ActionEvent a) 
 	{
@@ -64,30 +106,37 @@ public class WWheal extends JFrame implements MouseMotionListener,ActionListener
 		{
 			sshot=1;
 			System.out.println(sshot);
-			System.exit(0);
 		}
-		if(a.getSource()==b2)
+		else if(a.getSource()==b2)
 		{
 			sshot=2;
 			System.out.println(sshot);
 		}
-		if(a.getSource()==b3)
+		else if(a.getSource()==b3)
 		{
 			sshot=3;
 			System.out.println(sshot);
 		}
-		if(a.getSource()==b4)
+		else if(a.getSource()==b4)
 		{
 			sshot=4;
 			System.out.println(sshot);
 		}
-		if(a.getSource()==b5)
+		else if(a.getSource()==b5)
 		{
 			sshot=6;
 			System.out.println(sshot);
 		}
+		else if(a.getSource()==b6)
+		{
+			System.exit(0);
+		}
+		else if(a.getSource()==b7)
+		{
+			f1.setState(ICONIFIED);
+		}
 	}
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException
 	{
 		// TODO Auto-generated method stub
 		new WWheal();
@@ -98,12 +147,15 @@ public class WWheal extends JFrame implements MouseMotionListener,ActionListener
 		f1.setVisible(true);
 		f1.setSize(1920,1080);
 		f1.getContentPane().setLayout(null);
+		f1.getContentPane().add(b6);
+		f1.getContentPane().add(b7);
 		f1.getContentPane().add(l1);
 	}
 	public static void selectui(JFrame f2)
 	{
 		f2.setVisible(true);
-		f2.setSize(400,200);
+		f2.setSize(400,400);
+		f2.setLocation((960-200),(540-400));
 		f2.getContentPane().setLayout(null);
 		f2.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		f2.setTitle("Ball Type");
@@ -112,6 +164,10 @@ public class WWheal extends JFrame implements MouseMotionListener,ActionListener
 		f2.getContentPane().add(b3);
 		f2.getContentPane().add(b4);
 		f2.getContentPane().add(b5);
+		f2.getContentPane().add(r1);
+		f2.getContentPane().add(r2);
+		f2.getContentPane().add(r3);
+		f2.getContentPane().add(r4);
 		f2.getContentPane().add(l2);
 	}
 	public void mouseDragged(MouseEvent e) 
---
+++ src/main/java/httpRequestHandler/ReqHandler.java
@@ -0,0 +1,58 @@
+package main.java.httpRequestHandler;
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReqHandler extends main.java.MCG.WWheal
+{
+	public static void addShot(int playerID, String playerTeam, int playerSession, int scoreShot, float scoreX, float scoreY) throws IOException 
+	{
+
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				.build();
+		MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+		RequestBody body = RequestBody.create(mediaType, "playerID=" + playerID + "&playerTeam=" + playerTeam + "&playerSession=" + playerSession + "&scoreShot=" + scoreShot + "&scoreX=" + scoreX + "&scoreY=" + scoreY);
+		Request request = new Request.Builder()
+				.url("http://ec2-18-217-163-34.us-east-2.compute.amazonaws.com:8080/api/score")
+				.method("POST", body)
+				.addHeader("Content-Type", "application/x-www-form-urlencoded")
+				.build();
+		Response response = client.newCall(request).execute();
+	}
+	
+	public static ResponseBody playerList(String playerTeam) throws IOException 
+	{
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				.build();
+		Request request = new Request.Builder()
+				.url("http://ec2-18-217-163-34.us-east-2.compute.amazonaws.com:8080/api/player/" + playerTeam)
+				.method("GET", null)
+				.build();
+		Response response = client.newCall(request).execute();
+		return response.body();
+	}
+	
+	public static void main(String[] args) throws IOException 
+	{
+		//Json get from the Response body
+		System.out.println("Main Array : "+ playerList("MCG").string());
+		//Add Response body to the Json Array
+		JSONArray jarray=new JSONArray(playerList("MCG").string());
+		//Iterate on jArray to get Each players Details
+		for (int i = 0; i <jarray.length() ; i++) 
+		{	
+			//Get Each Player Object 
+			JSONObject jsonObject = jarray.getJSONObject(i);
+			//Print Each players Details get from the object
+			System.out.print(jsonObject.getInt("playerID"));
+			System.out.print("\t\t"+jsonObject.getString("playerName"));
+			System.out.println();			
+			//If you want you can create a Player Class and put all these details in to player type array list display those in the UI
+		}
+		addShot(1,"RCG", 1, 6, 100.0f, 20.5f);
+		String jsonObject=playerList("MCG").string();
+	}
+}
